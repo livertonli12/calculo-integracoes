326084244c8daaca6d89104bea93a38bbe7bad8e
Líverton Oliveira <lifelipe7@live.com>
2016-07-14 23:48:28 -0300

    Mensal e início do semanal

--- index.js
+++ index.js
@@ -1,56 +1,85 @@
 var moment = require('moment');
 var integration = require('./integracao_obj.js');
 integration.date_next_exec = null;
-
+var temp_next_exec = null;
 console.log("************************************************************");
 console.log("Iniciando execução, agora são: ", moment().format());
 console.log("************************************************************");
+integration.date_last_exec = moment("2016-07-15T00:00:00.00");
 
 if(integration.general_frequency){
+	
 	if (integration.general_frequency.frequency === 'daily') {
 
 		if (integration.daily_frequency) {
-			temp_next_exec = calcNextExecWithinDailyFrequency(integration.daily_frequency, temp_next_exec);
+			temp_next_exec = calcWithinDailyFrequency(integration.general_frequency, integration.daily_frequency, temp_next_exec);
 		} else {
 			if (integration.date_last_exec !== null) {
 				var last_exec = moment(integration.date_last_exec);
 
 				if (moment().get('date') === last_exec.get('date') && last_exec.isBefore(moment().endOf('day'))) {
-					//ainda não terminou o dia e estamos no mesmo dia.
 					integration.date_next_exec = moment().add(1, 'minute');
 				} else {
-					//já terminou o dia ... fudeu
-					integration.date_next_exec = moment().add(integration.general_frequency.recurrence, 'days');
+					integration.date_next_exec = moment().add(integration.general_frequency.recurrence, 'days')
+						.set({'hour':'00', 'minute':'00', 'second':'00'});
 				}
 			} else {
-				integration.date_next_exec = moment().add(integration.general_frequency.recurrece, 'days');
+				integration.date_next_exec = moment().format();
 			}
 		}		
 		
 	} else if (integration.general_frequency.frequency === 'weekly') {
-
-
-		///
+		//checar se tem frequencia diaria e se hoje é um dos dias em que a integração deve rodar.....
 		if (integration.daily_frequency) {
-			temp_next_exec = calcNextExecWithinDailyFrequency(integration.daily_frequency, temp_next_exec);
+			temp_next_exec = calcWithinDailyFrequency(integration.general_frequency, integration.daily_frequency, temp_next_exec);
+		} else {
+			if (integration.date_last_exec !== null) {
+				var last_exec = moment(integration.date_last_exec);
+
+				//checar se hoje é um dia da semana em que a integração deveria rodar e se o dia e a semana ainda nao terminaram
+				if (moment().get('date') === last_exec.get('date') && last_exec.isBefore(moment().endOf('day'))) {
+					integration.date_next_exec = moment().add(1, 'minute');
+				} else {
+					//garantir que a próxima data de execução será atribuida ao dia mais próximo dos que haviam sido configurados
+					//perante o dia de hoje.
+					integration.date_next_exec = moment().add(integration.general_frequency.recurrence, 'months')
+						.set({'hour':'00', 'minute':'00', 'second':'00'});
+				}
+			} else {
+				//garantir que a próxima data de execução será atribuida ao dia mais próximo dos que haviam sido configurados
+				//perante o dia de hoje.
+				integration.date_next_exec = moment().format();
+			}
 		}
-		///
-		
+
+
 	} else if (integration.general_frequency.frequency === 'monthly') {
-
-
-		///
 		if (integration.daily_frequency) {
-			temp_next_exec = calcNextExecWithinDailyFrequency(integration.daily_frequency, temp_next_exec);
+			temp_next_exec = calcWithinDailyFrequency(integration.general_frequency, integration.daily_frequency, temp_next_exec);
+		} else {
+			if (integration.date_last_exec !== null) {
+				var last_exec = moment(integration.date_last_exec);
+
+				if (moment().get('date') === last_exec.get('date') && last_exec.isBefore(moment().endOf('day'))) {
+					integration.date_next_exec = moment().add(1, 'minute');
+				} else {
+					integration.date_next_exec = moment().add(integration.general_frequency.recurrence, 'months')
+						.set({'hour':'00', 'minute':'00', 'second':'00'});
+				}
+			} else {
+				integration.date_next_exec = moment().format();
+			}
 		}
-		///
-		
 	}
 } else {
 	integration.date_next_exec = moment().add(1, 'minute');
 }
 
-function calcNextExecWithinDailyFrequency(general_frequency, daily_frequency, temp_next_exec){
+console.log(integration.date_next_exec.format());
+
+function calcWithinDailyFrequency(general_frequency, daily_frequency, temp_next_exec){
+	var next_exec = null;
+
 	var recurrence = integration.daily_frequency.recurrence;
 	var recurrence_unit = integration.daily_frequency.recurrence_unit;
 
@@ -75,22 +104,18 @@ function calcNextExecWithinDailyFrequency(general_frequency, daily_frequency, te
 	}
 	
 	if (integration.daily_frequency.recurrence !== null && integration.daily_frequency.recurrence_unit) {	
-		
-		temp_next_exec = moment(temp_next_exec).add(recurrence, recurrence_unit);
+
+		next_exec = moment().add(recurrence, recurrence_unit);
 
 		if (moment(temp_next_exec).isAfter(end_date)) {
-
-			console.log("************************************************************");
-			console.log("Ultrapassou o limite do dia, calculando novo dia de execução");
-			console.log("************************************************************");
-			temp_next_exec = moment(temp_next_exec).add(general_frequency.recurrence, 'days').set({'hour': start_date_hours, 'minute': start_date_minutes});
-		}
+			next_exec = moment().add(general_frequency.recurrence, 'days')
+									 .set({'hour': start_date_hours, 'minute': start_date_minutes});
+		} 
 
 	} else {
-		
-		temp_next_exec = moment(temp_next_exec).add(1, 'minutes');
-
+		next_exec = moment(temp_next_exec).add(1, 'minutes');
 	}
 	
-	return temp_next_exec;
+	console.log("Próxima execução em: ", next_exec.format());
+	return next_exec;
 }
